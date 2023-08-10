016ce9827c14a1e4bda3290dc8767d16b8790c10
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-08-10 18:54:05 -0400

    fix /version and /health routes to come before express.static middleware

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
-
 app.get('/health', (req, res) => {
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('1')
+  res.send('10')
 })
+
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
